fcae0660bb0d871098d1f10c02fe39e3829f5f28
Tanya <120472619+tacherenko@users.noreply.github.com>
2023-08-01 16:57:06 +0200

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports =  function reverse (n) {
+    const reversed = parseInt(n.toString().split('').reverse().join(''));
+    return reversed;
 }
